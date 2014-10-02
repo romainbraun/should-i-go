1632363aec6e86a2819bc03e4ba3b4a3e82ca6d9
romainbraun <braunromain@romains-air.gateway.2wire.net>
2014-10-02 00:33:06 -0700

    Cloudfront

--- v2/gulpfile.js
+++ v2/gulpfile.js
@@ -6,6 +6,10 @@ var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
+var awspublish = require('gulp-awspublish');
+var aws = require('./aws.json');
+
+// var aws = JSON.parse(awsInfos);
 
 var getBundleName = function () {
   var version = require('./package.json').version;
@@ -33,4 +37,32 @@ gulp.task('javascript', function() {
   };
 
   return bundle();
+});
+
+
+gulp.task('publish', function() {
+
+  // create a new publisher
+  var publisher = awspublish.create({ key: aws.key,  secret: aws.secret, bucket: aws.bucket });
+
+  // define custom headers
+  var headers = {
+     'Cache-Control': 'max-age=315360000, no-transform, public'
+     // ...
+   };
+
+  return gulp.src('./assets/js/dist/*.js')
+
+     // gzip, Set Content-Encoding headers and add .gz extension
+    .pipe(awspublish.gzip({ ext: '.gz' }))
+
+    // publisher will add Content-Length, Content-Type and  headers specified above
+    // If not specified it will set x-amz-acl to public-read by default
+    .pipe(publisher.publish(headers))
+
+    // create a cache file to speed up consecutive uploads
+    .pipe(publisher.cache())
+
+     // print upload updates to console
+    .pipe(awspublish.reporter());
 });
