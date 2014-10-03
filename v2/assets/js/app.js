210126edcc379c096ab32e77c146eb1397975d0c
weyland-dev <weyland@Weylands-MacBook-Pro.local>
2014-10-02 17:24:23 -0700

    Weyland progress

--- v2/assets/js/app.js
+++ v2/assets/js/app.js
@@ -1,3 +1,35 @@
-var test = "ok";
 var facebookAPI = require('./facebook.js');
-console.log(facebookAPI);
+var $ = require('jquery');
+
+
+function fbLoaded() {
+	facebookAPI.init(function () {
+		FB.api('/me', function(response) {
+	      console.log('Successful login for: ' + response.name);
+	    });
+	});
+}
+
+window.fbAsyncInit = function() {
+	FB.init({
+		appId      : '305479839616788',
+		cookie     : true,
+		xfbml      : true,  
+		version    : 'v2.1' 
+	});
+	
+	fbLoaded();
+};
+
+$('.compute').click(function () {
+	var eventId = $('input').val();
+
+	eventId = eventId.substring(eventId.indexOf('events/')+7);
+	eventId = eventId.substring(eventId.indexOf('/'), 0);
+	
+	facebookAPI.getEventInfos(eventId, function (response) {
+		console.log(response);
+		$('.step1').removeClass().addClass('step2');
+	});
+});
+
