1c98229013ae568c62ab70bd73d7ebfa2300623c
Romain Braun <braun.rom@gmail.com>
2014-10-17 00:28:00 -0700

    Might want to ignore that unit-test-coverage folder!

--- assets/js/test/test.js
+++ assets/js/test/test.js
@@ -1,9 +1,10 @@
 var assert = require("assert");
 var app = require('../utils.js');
+
 describe('Test', function(){
   describe('getMonth', function(){
     it('should return Oct', function(){
-      assert.equal(false, app.getMonth(new Date()));
+      assert.equal('Oct', app.getMonth(new Date('Thu Oct 16 2014')));
     });
   });
 });
--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,8 +1,10 @@
 /*globals module */
 (function () {
 	'use strict';
+	var monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-	// This is using ECMAScript 5.1's Internationalization API. This will fail on <IE11. And it fails on Node!
+
+	// This is using ECMAScript 5.1's Internationalization API. This will fail on <IE11. And it fails on Node! Let's go with the old fashioned way
 	// var formatter = new Intl.DateTimeFormat("en-us", { month: "short" });
 
 	/**
@@ -11,7 +13,7 @@
 	 * @return {String}
 	 */
 	module.exports.getMonth = function (date) {
-		return false;
+		return monthNames[date.getMonth()];
 		// return formatter.format(date);
 	};
 
